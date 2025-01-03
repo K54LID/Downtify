cfa3f2d5b6019d60d45fa1b4effb68b8534b9e34
Khalid <iamxalid@gmail.com>
2025-01-03 22:41:33 +0300

    m

--- src/constants.ts
+++ src/constants.ts
@@ -6,17 +6,17 @@ export const t = {
 		"Bot is currently under maintenance, it'll return shortly.",
 	processing: "Processing...",
 	deniedMessage: [
-		bold("Introducing Downtify – Your Ultimate Video Downloader 📱✨"),
+		bold("Introducing Downtify – Your Ultimate Video Downloader"),
 		"",
 		"Say goodbye to the hassle of downloading videos from YouTube, TikTok, and Instagram! With Downtify, you can easily and quickly download videos with just a link. It’s simple, fast, and convenient!",
 		link("Why Choose Downtify?", "https://t.me/downtify_bot"),
 		"",
 		bold(
-			`✅ Fast and seamless video downloads
-			)} ✅ Supports YouTube, TikTok, and Instagram
-			)} ✅ Easy to use – just send the link!
-			)} ✅ Private and secure – only available to subscribers
-			)} ✅ All videos are downloaded watermark-free`
+			`  *Fast and seamless video downloads
+			)} * Supports YouTube, TikTok, and Instagram
+			)} * Easy to use – just send the link!
+			)} * Private and secure – only available to subscribers
+			)} * All videos are downloaded watermark-free`
 		),
 		"",
 		bold("Subscription Details"),
